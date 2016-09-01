e48e190818f7449357de05b65025c82da4db659c
ale <alexcoppe@gmail.com>
2016-09-01 14:33:45 +0200

    First version of the mutect project. Still need to implment variants annotation

---
+++ prj/mutect/settings.py
@@ -0,0 +1,25 @@
+#The reference genome in fasta format
+reference = "reference.fa"
+referenceDir = "./genome"
+
+
+#Bam files to be used
+bamN = "m1.bam"
+bamT = "m2.bam"
+
+
+#Annotation directory
+annotationDir = "./annotation"
+#Exome sequencing kit bed file
+exomeRegions = "nexterarapidcapture_exome_targetedregions_v1.2.bed"
+
+#Directory containing  muTect-1.1.5.jar
+mutectDir = "~/local/mutect/"
+
+#The dbSNP vcf file
+dbsnpVCF = "All_20151104.vcf"
+
+
+##Computational resources
+#processors = 8
+#maxMemory = 1000000000
