d9b5a5371b6edac5b8370cd0d95ad35104ac744a
ale <alexcoppe@gmail.com>
2016-09-06 16:06:07 +0200

    First working version of mutect2-split-chr project

---
+++ prj/mutect2-split-chr/settings.py
@@ -0,0 +1,34 @@
+#The reference genome in fasta format
+reference = "reference.fa"
+referenceDir = "~/genomic_data/human_genome/"
+
+##The paired reads in fastq format
+#reads1 = "m1_TAAGGCGA_R1_M.fastq.gz"
+#reads2 = "m1_TAAGGCGA_R2_M.fastq.gz"
+
+#Annotation directory
+annotationDir = "~/annotation/"
+#Exome sequencing kit bed file
+exomeRegions = "nexterarapidcapture_exome_targetedregions_v1.2.bed"
+
+##Directory containing  GenomeAnalysisTK.jar  
+#gatkJarDir = "~/local/GenomeAnalysisTK/"
+
+##The dbSNP vcf file
+#dbsnpVCF = "All_20151104.vcf"
+
+##Project metadata
+#projectName = "m1"
+#sampleName = "m1"
+
+##Computational resources
+#processors = 8
+#maxMemory = 1000000000
+
+#The bam from the tumoreal sample
+tumorBam = "t_22.bam"
+normalBam = "n_22.bam"
+
+
+startChr = 21
+endChr = 22
