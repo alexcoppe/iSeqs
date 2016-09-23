5cb66e7e58ff60849f23f70923ed2170ca4937c4
ale <alexcoppe@gmail.com>
2016-09-23 17:36:48 +0200

    Now vcf_2_table.py prints header and lists available fields with the -e option

--- src/vcf-tools/vcf_2_table.py
+++ src/vcf-tools/vcf_2_table.py
@@ -134,12 +134,29 @@ def getStuff(d,k):
         return [el.get(k) for el in d]
     return d.get(k)
 
+#Print tabulated version of from_splitted_variants_to_transcripts function
 def print_transcripts(transcripts, fields = ["variant:chr", "variant:pos", "variant:ref",  "variant:alt" , "transcript:impact", "samples:AD", "samples:DP", "transcript:gene_name", "transcript:feature"]):
     lines_to_print = []
     for transcript in transcripts:
         stuff_to_print_original_type = [reduce(getStuff, field.split(":"), transcript) for field in fields ]
         print "\t".join([str(el) if not isinstance(el, list) else "\t".join([str(subel) for subel in el]) for el in stuff_to_print_original_type])
         
+
+def list_fields(transcripts):
+    print "Available fields and subfields are:"
+    transcript = transcripts[0]
+    first_level_fields = transcript.keys()
+    for field in first_level_fields:
+        second_level_keys = transcript.get(field)
+        if second_level_keys:
+            print "{}".format(field)
+            for second_level_field in  second_level_keys:
+                if second_level_field.__class__ == {}.__class__:
+                    for k in second_level_field.keys():
+                        print "  - :{}".format(k)
+                else:
+                    print "  - :{}".format(second_level_field)
+    print "Use -e \"field:subfield,field:subfield\" (e.g. \"variant:chr,variant:pos\") \" to specify with subfields to display"
 
 
 #Given a FORMAT field as string (e.g. GT:AD:BQ:DP:FA:SS)
@@ -164,20 +181,55 @@ def get_vcf_fixed_fields(vcf_line):
     return fields
 
 
+def print_header(vcf, fields):
+    for line in vcf:
+        if not line.startswith("#"):
+            variant_fields = get_vcf_fixed_fields(line)
+            number_of_samples = len( variant_fields.get("sample_fields") )
+            break
+    header = []
+    for field_subfield in fields:
+        field,subfield = field_subfield.split(":")
+        if field == "samples":
+            samples = range(1, number_of_samples + 1)
+            [header.append("{}_{}".format(s,subfield) ) for s in samples]
+        else:
+            header.append(subfield)
+    header = "\t".join(header)
+    vcf.seek(0)
+    print header
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Do something")
 
     parser.add_argument('-f', '--vcf', action='store', type=file, help="vcf file", required=True)
-    parser.add_argument("-p", "--patient", help="Patient name to be printed in patient column", required=False, type=str, default="")
+    parser.add_argument("-p", "--patient", help="Patient name to be printed in patient column. If not specified, no patient column will be shown", required=False, type=str, default="")
     parser.add_argument("-i", "--impacts", help="List of impacts to be filtered, separated by ,", required=False, type=str, default="")
+    parser.add_argument("-e", "--fields", help="Fields from the vcf to be printed. Use -l option to list the available fields. Fields should be specified as \"field:subfield,field:subfield:.....\"", required=False, type=str, default="")
+    parser.add_argument("-l", "--list", help="List available fields to print. To use in combination with the -f option to inspect the fields and subfields available in the vcf", required=False, action="store_true")
+
     args = parser.parse_args()
 
     patient = args.patient
     impacts = args.impacts.split(",")
+    list_fields_option = args.list
+    
+    if args.fields:
+        fields = args.fields.split(",")
+    else:
+        fields = ["variant:chr", "variant:pos", "variant:ref",  "variant:alt" , "transcript:impact", "samples:AD", "samples:DP", "transcript:gene_name", "transcript:feature"]
 
+    if not list_fields_option:
+        print_header(args.vcf, fields)
 
     for line in  args.vcf:
         if not line.startswith("#"):
             variants_in_line = split_vcf_line_by_alt(line)
             transcripts_with_variants = from_splitted_variants_to_transcripts(variants_in_line)
-            print_transcripts(transcripts_with_variants)
+            if list_fields_option:
+                list_fields(transcripts_with_variants)
+                sys.exit()
+
+            print_transcripts(transcripts_with_variants, fields)
