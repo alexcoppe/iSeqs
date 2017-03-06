27d4e7b3ac6c6237b5dd29baefca247e09b407b6
Ale <alexcoppe@gmail.com>
2017-03-06 11:06:03 +0100

    clean.sh from bwa-mem-single-sample project now removes files with reads without Ns

--- prj/bwa-mem-single-sample/clean.sh
+++ prj/bwa-mem-single-sample/clean.sh
@@ -5,7 +5,7 @@ echo -n "Are you sure you want to remove all intermediate files (y/n)? "
 read yes
 
 if [ "$yes" == "y" ]; then
-    rm -f *.pyc *.grp mapping.bam mapping-sorted.bam mapping-sorted-rmdup.bam  mapping-sorted-rmdup.bam.bai  mapping-sorted-rmdup-realigned.bai mapping-sorted-rmdup-realigned.bam realigning.intervals
+    rm -f *.pyc *.grp mapping.bam mapping-sorted.bam mapping-sorted-rmdup.bam  mapping-sorted-rmdup.bam.bai  mapping-sorted-rmdup-realigned.bai mapping-sorted-rmdup-realigned.bam realigning.intervals r1_no_Ns.fastq.gz r2_no_Ns.fastq.gz
     exit 1
 else
     exit 1
